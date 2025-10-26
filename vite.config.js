fd98e4049e7e77fbadbe562a18937812572373e1
jamilnim <jamilnimbook2@gmail.com>
2025-10-26 10:39:32 +0200

    nn

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: {
-      react: path.resolve("./node_modules/react"),
-      "react-dom": path.resolve("./node_modules/react-dom"),
+    dedupe: ["react", "react-dom", "react-router-dom"], // ✅ ensures single version
+  },
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      external: [],
     },
   },
 });
